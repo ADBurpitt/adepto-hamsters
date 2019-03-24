976bbea34543259b8d0acfea89b7c06f77d90e7c
ADBurpitt <aaronburpitt@gmail.com>
2019-03-24 23:26:08 +0000

    Add like post function

---
+++ backend/src/like-post/app.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk')
+const nanoid = require('nanoid')
+
+exports.lambdaHandler = async (event, context) => {
+  const body = JSON.parse(event.body);  
+  const docClient = new AWS.DynamoDB.DocumentClient()
+
+  try {
+
+    const post = await docClient.get({
+      TableName: process.env.TABLE_NAME,
+      Key: { HashKey: body.postId }
+    }).promise()
+
+    console.log(post)
+
+    return {
+      'statusCode': 200,
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      'body': JSON.stringify({ })
+    }
+  } catch (error) {
+    return {
+      'statusCode': 502,
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      'body': JSON.stringify({ 'message': err })
+    }
+  }
+}
