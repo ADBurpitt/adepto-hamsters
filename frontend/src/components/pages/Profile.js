25f4387d24766b3b2b055a5bcb6e1610490da636
ADBurpitt <aaronburpitt@gmail.com>
2019-03-25 23:23:14 +0000

    Convert to functional component, add labels

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import { Container, Row, Col, Jumbotron } from 'reactstrap'
 
-class Profile extends Component {
-  state = {  }
-  
-  render() {
-    return (
-      <Container>
-        <Jumbotron className="margin-top-100">
-          <Container className="text-center" >
-            <Row>
-              <Col>
-                <h1>{ this.props.user.username }</h1>
-              </Col>
-            </Row>
-            <Row className="margin-top-25" >
-              <Col>
-                <h3>{ this.props.user.attributes.email }</h3>
-              </Col>
-            </Row>
-          </Container>
-        </Jumbotron>
+const Profile = ({user}) =>
+  <Container>
+    <Jumbotron className="margin-top-100">
+      <Container className="text-center" >
+        <Row>
+          <Col>
+            <h1>Username: { user.username }</h1>
+          </Col>
+        </Row>
+        <Row className="margin-top-25" >
+          <Col>
+            <h3>Email: { user.attributes.email }</h3>
+          </Col>
+        </Row>
       </Container>
-    )
-  }
-}
+    </Jumbotron>
+  </Container>
+
 
 export default connect(
   state => ({ user: state.auth.user }),
