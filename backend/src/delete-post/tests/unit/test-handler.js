e5ac81694ffd4c180df96746c1d15a37698c0b24
ADBurpitt <aaronburpitt@gmail.com>
2019-03-25 22:59:37 +0000

    Cleanup unused test files

--- backend/src/delete-post/tests/unit/test-handler.js
+++ backend/src/delete-post/tests/unit/test-handler.js
@@ -5,17 +5,6 @@ const chai = require('chai');
 const expect = chai.expect;
 var event, context;
 
-describe('Tests index', function () {
-    // it('verifies successful response', async () => {
-    //     const result = await app.lambdaHandler(event, context)
+describe('Delete Post', function () {
 
-    //     expect(result).to.be.an('object');
-    //     expect(result.statusCode).to.equal(200);
-    //     expect(result.body).to.be.an('string');
-
-    //     let response = JSON.parse(result.body);
-
-    //     expect(response).to.be.an('object');
-    //     expect(response.message).to.be.equal("hello world");
-    // });
 });
