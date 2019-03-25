818714d71aedc27606cc07d86a8cc8f5dfa7a223
ADBurpitt <aaronburpitt@gmail.com>
2019-03-25 23:22:45 +0000

    Add timeline page

---
+++ frontend/src/components/pages/Timeline/Post.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { toggleLike } from 'api'
+
+import { Container, Row, Col, Button } from 'reactstrap'
+import { ImgButton } from 'style/components'
+import trash from 'assets/trash.svg'
+
+const Post = ({uuid, userId, title, text, likes, email, timestamp, deletePost}) => {
+  const [likeState, setLikes] = useState(likes)
+  const date = new Date(timestamp)
+
+  return (
+    <Container>
+      <Row>
+        <Col sm={11}>
+          <h1 className="display-5">{title} <span style={{fontSize: 12}} className="align-middle text-muted">{email} - {date.toLocaleDateString()}</span></h1>
+        </Col>
+        {/* <Col sm={3}><span className="align-middle text-muted">{email} - {date.toLocaleDateString()}</span></Col> */}
+        <Col sm={1}><ImgButton src={trash} alt="delete" onClick={() => deletePost(uuid)}/></Col>
+      </Row>
+      <Row>
+        <Col><p className="lead">{text}</p></Col>
+      </Row>
+      <Row>
+        <Col sm={10}><span className="align-middle text-muted">{likeState.length}</span></Col>
+        <Col>
+          <Button
+            size="sm"
+            color={likeState.includes(userId) ? "danger" : "success"}
+            onClick={async () => {
+                try {
+                  const { data: { Attributes } } = await toggleLike(uuid)
+                  setLikes(Attributes.likes)
+                } catch (error) {
+                  console.error(error)
+                }
+              }
+            }>
+            {likeState.includes(userId) ? "unlike" : "like"}
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+
+export default Post
---
+++ frontend/src/components/pages/Timeline/Timeline.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts, deletePost } from 'api'
+
+import { Container, Row, Col, Jumbotron } from 'reactstrap'
+import { AddButton } from 'style/components'
+import Post from './Post'
+
+export class Timeline extends Component {
+  state = { items: [] }
+
+  async componentDidMount() {
+    if (this.props.items) { // For testing, kinda hacky
+      this.setState({ ...this.props }) 
+    } else {
+      try {
+        const { Items } = await fetchPosts()
+        console.log(Items)
+        this.setState({ items: Items.sort((a, b) => a.timestamp < b.timestamp) })
+      } catch (error) {
+        console.error(error)
+      }      
+    }
+  }
+
+  deletePost = async postId => {
+    try {
+      const data = await deletePost(postId)
+      console.log(data)
+      this.setState({ items: this.state.items.filter(item => item.uuid !== postId) })
+    } catch (error) {
+      console.error(error) 
+    }
+  }
+
+  render() {
+    const { user, loading } = this.props.auth
+    return (
+      <Container>
+        <Jumbotron className="margin-top-100">
+          <Container className="text-center">
+            <h1 className="display-3">Greetings, fellow hamsters!</h1>
+            <AddButton onClick={() => this.props.history.push('/posts/new')} >Create Post</AddButton>
+            <hr className="my-2" />
+          </Container>
+          
+          <Container className="margin-top-10" >
+            <Row>
+              <Col>
+                {
+                  this.state.items.map((item, i) =>
+                    <React.Fragment key={item.uuid}>
+                      <Post
+                        {...item}
+                        deletePost={this.deletePost}
+                        userId={!loading && user && user.attributes.sub} />
+                      { i !== (this.state.items.length - 1) && <hr className="my-2" />}
+                    </React.Fragment>
+                  )
+                }
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+      </Container>
+    )
+  }
+}
+
+export default connect(
+  store => ({ auth: store.auth }),
+  null
+)(Timeline)
