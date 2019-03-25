58c690ac7463adac363c32b52107f6e2552df58f
ADBurpitt <aaronburpitt@gmail.com>
2019-03-25 19:40:20 +0000

    Add delete post function

---
+++ backend/src/delete-post/app.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk')
+const nanoid = require('nanoid')
+
+exports.lambdaHandler = async (event, context) => {
+  const sub = event.requestContext.authorizer.claims.sub
+  const body = JSON.parse(event.body);  
+  const documentClient = new AWS.DynamoDB.DocumentClient()
+
+  try {
+    const data = await documentClient.delete({
+      TableName: process.env.TABLE_NAME,
+      Key: { Key: { uuid: body.postId } },
+      ConditionExpression: "user_id == :sub",
+      ExpressionAttributeValues: { ":sub": sub },
+      ReturnValues: "ALL_OLD"
+    }).promise()
+
+  return {
+      'statusCode': 200,
+      'headers': {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*"
+      },
+      'body': JSON.stringify(data)
+    }
+  } catch (error) {
+    return {
+      'statusCode': 502,
+      'headers': {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*"
+      },
+      'body': JSON.stringify({ 'message': error })
+    }
+  }
+}
